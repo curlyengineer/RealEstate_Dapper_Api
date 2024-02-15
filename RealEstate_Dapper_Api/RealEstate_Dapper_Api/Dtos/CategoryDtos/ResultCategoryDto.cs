b088105517a3ded0c84cf8de68dc0cd54cfd2bf0
minenur.kaya <minenur.kaya@gmail.com>
2024-02-15 16:37:50 +0300

    Create Models, Dtos, Repository Files. download Microsoft.Data.SqlClient and Dapper. #

---
+++ RealEstate_Dapper_Api/RealEstate_Dapper_Api/Dtos/CategoryDtos/ResultCategoryDto.cs
@@ -0,0 +1,9 @@
+﻿namespace RealEstate_Dapper_Api.Dtos.CategoryDtos
+{
+    public class ResultCategoryDto
+    {
+        public int CategoryID { get; set; }
+        public string CategoryName { get; set; }
+        public bool CategoryStatus { get; set; }  // veritabanında bit olarak tutulan verilen VS de bool olarak tutulur 
+    }
+}
