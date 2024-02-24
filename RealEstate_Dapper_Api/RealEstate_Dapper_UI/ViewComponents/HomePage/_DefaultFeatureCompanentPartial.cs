804195214a49aa25168c0b43dda72493fb9201d8
Mine Nur KAYA <minenur.kaya@gmail.com>
2024-02-24 22:05:58 +0300

    Added Home Partial Views

---
+++ RealEstate_Dapper_Api/RealEstate_Dapper_UI/ViewComponents/HomePage/_DefaultFeatureCompanentPartial.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Security.Permissions;
+
+namespace RealEstate_Dapper_UI.ViewComponents.HomePage
+{
+    public class _DefaultFeatureCompanentPartial :ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+    }
+}
